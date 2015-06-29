4a63914aa65c16e4f544ae49958f80208c49224e
Jérôme Martinez <jerome@mediaarea.net>
2015-06-29 21:46:13 +0200

    Updated Visual Studio project for handling latest additions

--- Project/MSVC2013/GUI/qt_update.bat
+++ Project/MSVC2013/GUI/qt_update.bat
@@ -1,4 +1,8 @@
 mkdir _Automated
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\mainwindow.h -o_Automated\moc_mainwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesedit.h -o_Automated\moc_policiesedit.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesmenu.h -o_Automated\moc_policiesmenu.cpp
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\mainwindow.ui -o _Automated\ui_mainwindow.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policiesedit.ui -o _Automated\ui_policiesedit.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policiesmenu.ui -o _Automated\ui_policiesmenu.h
 ..\..\..\..\Qt\qtbase\bin\rcc ..\..\..\Source\Resource\Resources.qrc -o _Automated\qrc_Resources.cpp
