67294f11e3d00a3b2613ca99ab51ccce067007e1
Jérôme Martinez <jerome@mediaarea.net>
2015-07-10 21:46:12 +0200

    Visual C++ project file update in order to support latest code updates

--- Project/MSVC2013/GUI/qt_update.bat
+++ Project/MSVC2013/GUI/qt_update.bat
@@ -2,7 +2,17 @@ mkdir _Automated
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\mainwindow.h -o_Automated\moc_mainwindow.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesedit.h -o_Automated\moc_policiesedit.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesmenu.h -o_Automated\moc_policiesmenu.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\groupofrules.h -o_Automated\moc_groupofrules.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiestree.h -o_Automated\moc_policiestree.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policymenu.h -o_Automated\moc_policymenu.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\ruleedit.h -o_Automated\moc_ruleedit.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\rulemenu.h -o_Automated\moc_rulemenu.cpp
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\mainwindow.ui -o _Automated\ui_mainwindow.h
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policiesedit.ui -o _Automated\ui_policiesedit.h
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policiesmenu.ui -o _Automated\ui_policiesmenu.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\groupofrules.ui -o _Automated\ui_groupofrules.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policiestree.ui -o _Automated\ui_policiestree.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policymenu.ui -o _Automated\ui_policymenu.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\ruleedit.ui -o _Automated\ui_ruleedit.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\rulemenu.ui -o _Automated\ui_rulemenu.h
 ..\..\..\..\Qt\qtbase\bin\rcc ..\..\..\Source\Resource\Resources.qrc -o _Automated\qrc_Resources.cpp
