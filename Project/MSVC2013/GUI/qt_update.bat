2697bd1b6cbd49bf30b0bf32816fe9f999046a51
Jérôme Martinez <jerome@mediaarea.net>
2015-10-15 20:33:49 +0200

    Update of Visual C++ projects for compiling new files from previous commits.

--- Project/MSVC2013/GUI/qt_update.bat
+++ Project/MSVC2013/GUI/qt_update.bat
@@ -1,7 +1,17 @@
 mkdir _Automated
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\mainwindow.h -o_Automated\moc_mainwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\helpwindow.h -o_Automated\moc_helpwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\menumainwindow.h -o_Automated\moc_menumainwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\checkerwindow.h -o_Automated\moc_checkerwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policieswindow.h -o_Automated\moc_policieswindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policywindow.h -o_Automated\moc_policywindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\schematronwindow.h -o_Automated\moc_schematronwindow.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\xsltpolicymenu.h -o_Automated\moc_xsltpolicymenu.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\xsltruleedit.h -o_Automated\moc_xsltruleedit.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\xsltwindow.h -o_Automated\moc_xsltwindow.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesedit.h -o_Automated\moc_policiesedit.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiesmenu.h -o_Automated\moc_policiesmenu.cpp
+..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policymenu.h -o_Automated\moc_policymenu.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\groupofrules.h -o_Automated\moc_groupofrules.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policiestree.h -o_Automated\moc_policiestree.cpp
 ..\..\..\..\Qt\qtbase\bin\moc ..\..\..\Source\GUI\Qt\policymenu.h -o_Automated\moc_policymenu.cpp
@@ -17,4 +27,6 @@ mkdir _Automated
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\policymenu.ui -o _Automated\ui_policymenu.h
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\ruleedit.ui -o _Automated\ui_ruleedit.h
 ..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\rulemenu.ui -o _Automated\ui_rulemenu.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\xsltpolicymenu.ui -o _Automated\ui_xsltpolicymenu.h
+..\..\..\..\Qt\qtbase\bin\uic ..\..\..\Source\GUI\Qt\xsltruleedit.ui -o _Automated\ui_xsltruleedit.h
 ..\..\..\..\Qt\qtbase\bin\rcc ..\..\..\Source\Resource\Resources.qrc -o _Automated\qrc_Resources.cpp
