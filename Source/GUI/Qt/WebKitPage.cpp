4bed8d807a878da8bceecd2d2da27f47cd5cd4d3
Guillaume Roques <guillaume.roques@gmail.com>
2016-01-27 10:25:45 +0100

    Remove hardcoded charset in html output
    Signed-off-by: Guillaume Roques <guillaume.roques@gmail.com>

--- Source/GUI/Qt/WebKitPage.cpp
+++ Source/GUI/Qt/WebKitPage.cpp
@@ -77,7 +77,6 @@ namespace MediaConch
         if (dl_file.endsWith(".html"))
         {
             out << "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n";
-            out << "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
             out << report;
         }
         else
