b65d719b57e52f0a768921025dbace3a1c1a2dab
Florent Tribouilloy <florent@mediaarea.net>
2016-04-27 19:33:53 +0200

    test: Improve test cases
    Signed-off-by: Florent Tribouilloy <florent@mediaarea.net>

--- Project/GNU/CLI/test/test_ffv1.sh
+++ Project/GNU/CLI/test/test_ffv1.sh
@@ -3,9 +3,66 @@
 PATH_SCRIPT=$(dirname "$0")
 . "$PATH_SCRIPT/utils.sh"
 
-FILES=`ls $PATH_SCRIPT/SampleFiles/PolicyTestFiles/FFV1/`
+FILES_DIRECTORY="$PATH_SCRIPT/SampleFiles/PolicyTestFiles/FFV1/"
+FILES="`ls "$FILES_DIRECTORY"`"
 
 for i in $FILES
 do
-    cmd_is_ok "./mediaconch --implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml $i"
+
+    OUTPUT="`./mediaconch \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_invalid "$OUTPUT"
+    output_has_not_mc_xml "$OUTPUT"
+    output_has_not_mi_xml "$OUTPUT"
+    output_has_not_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mc -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_mc_xml "$OUTPUT"
+    output_has_not_mi_xml "$OUTPUT"
+    output_has_not_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mi -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_not_mc_xml "$OUTPUT"
+    output_has_mi_xml "$OUTPUT"
+    output_has_not_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mt -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_not_mc_xml "$OUTPUT"
+    output_has_not_mi_xml "$OUTPUT"
+    output_has_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mt -mi -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_not_mc_xml "$OUTPUT"
+    output_has_mi_xml "$OUTPUT"
+    output_has_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mc -mi -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_mc_xml "$OUTPUT"
+    output_has_mi_xml "$OUTPUT"
+    output_has_not_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mc -mt -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_mc_xml "$OUTPUT"
+    output_has_not_mi_xml "$OUTPUT"
+    output_has_mt_xml "$OUTPUT"
+
+    OUTPUT="`./mediaconch -mt -mi -mc -fx \"--implementationschema=$RESOURCE_PATH/schema/MatroskaSchema.xml\" \"$FILES_DIRECTORY/$i\"`"
+    cmd_is_ok
+    xml_is_correct "$OUTPUT"
+    output_has_mc_xml "$OUTPUT"
+    output_has_mi_xml "$OUTPUT"
+    output_has_mt_xml "$OUTPUT"
+
 done
--- Project/GNU/CLI/test/utils.sh
+++ Project/GNU/CLI/test/utils.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 UTILS_PATH=$(dirname "$0")
-MC_ROOT_PATH=$UTILS_PATH/../../../
-RESOURCE_PATH=$MC_ROOT_PATH/Resource
+MC_ROOT_PATH=$UTILS_PATH/../../../../
+RESOURCE_PATH=$MC_ROOT_PATH/Source/Resource
 
 cmd_is_ok()
 {
-    CMD=$1
-    RESULT=`$CMD`
-
     if test $? -ne 0
     then
         exit 1;
@@ -17,10 +14,73 @@ cmd_is_ok()
 
 cmd_is_ko()
 {
-    CMD=$1
-    RESULT=`$CMD`
-
     if test $? -eq 0
+    then
+        exit 1;
+    fi
+}
+
+xml_is_correct()
+{
+    $(echo "$1" | xmllint --noout - 2> /dev/null)
+    if test $? -ne 0
+    then
+        exit 1;
+    fi
+}
+
+xml_is_invalid()
+{
+    $(echo "$1" | xmllint --noout - 2> /dev/null)
+    if test $? -eq 0
+    then
+        exit 1;
+    fi
+}
+
+output_has_mc_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaConch") -eq 0 ]
+    then
+        exit 1;
+    fi
+}
+
+output_has_not_mc_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaConch") -ne 0 ]
+    then
+        exit 1;
+    fi
+}
+
+output_has_mt_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaTrace") -eq 0 ]
+    then
+        exit 1;
+    fi
+}
+
+output_has_not_mt_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaTrace") -ne 0 ]
+    then
+        exit 1;
+    fi
+}
+
+output_has_mi_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaInfo") -eq 0 ]
+    then
+        exit 1;
+    fi
+}
+
+output_has_not_mi_xml()
+{
+    if [ $(echo "$1" | grep -oc "<MediaInfo") -ne 0 ]
     then
         exit 1;
     fi
