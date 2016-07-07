7f6a69aabd1471088f5b80ee13bbcadf9b8ea2b1
Florent Tribouilloy <florent@mediaarea.net>
2016-07-07 09:19:41 +0200

    WUI/Policies: try to find web parameters structure
    Signed-off-by: Florent Tribouilloy <florent@mediaarea.net>

--- Source/Resource/html/js/policyWebEngine.js
+++ Source/Resource/html/js/policyWebEngine.js
@@ -6,25 +6,56 @@ function getPolicyTreeData() {
 }
 
 function policyImportForm(form) {
+    // webpage.import_policy(form, function (data) {
+    // //     data: policyName, policyId, policyRules
+    //     policyImport(data);
+    // });
 }
 
 function policyCreateForm(form) {
+    // webpage.create_policy(form, function (data) {
+    // //     data: policyName, policyId
+    //     policyCreate(data);
+    // });
 }
 
 function policyRuleForm(form, ruleNode, action, routeAction) {
+    // webpage.rule_action(routeAction, policyId, ruleNode.data.Id, action, function (data) {
+    //     ruleAction(data, ruleNode, action);
+    // });
 }
 
 function policyDuplicateRequest(policyNode) {
+    // webpage.duplicate_policy(policyNode, function (data) {
+    // // data: policyName, policyId, policyRules
+    //     policyDuplicate(data, policyNode);
+    // });
 }
 
 function policyDeleteRequest(policyNode) {
+    // webpage.delete_policy(policyNode, function (data) {
+    // // data: 
+    //     policyDelete(data, policyNode);
+    // });
 }
 
 function policyRuleCreateRequest(policyNode) {
+    // webpage.create_rule(policyNode, function (data) {
+    // // data: ...
+    //     policyRuleCreate(data, policyNode);
+    // });
 }
 
 function getFieldsList(trackType, field) {
+    // webpage.get_fields_list(trackType, field, function (data) {
+    // // data: [{key,value}]
+    //     fieldsListOk(data, field);
+    // });
 }
 
 function getValuesList(trackType, field, value) {
+    // webpage.get_values_list(trackType, field, value, function (data) {
+    // // data: [{key,value}]
+    //     valuesListOk(data.values, value);
+    // });
 }
