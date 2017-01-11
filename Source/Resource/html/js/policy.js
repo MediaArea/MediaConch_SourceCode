69389575024ee3f81b9ecedf9e5e5ed62f386995
Guillaume Roques <guillaume.roques@gmail.com>
2017-01-11 11:26:20 +0100

    Split lines to avoid lintian error
    Signed-off-by: Guillaume Roques <guillaume.roques@gmail.com>

--- Source/Resource/html/js/policy.js
+++ Source/Resource/html/js/policy.js
@@ -313,22 +313,56 @@ function policyEditHelp() {
     };
 
     // Name
-    addHelp('policyName', 'Brief summary of policy purpose.', 'Policy name');
+    addHelp(
+        'policyName',
+        'Brief summary of policy purpose.',
+        'Policy name'
+    );
 
     // Description
-    addHelp('policyDescription', 'Optional description of the intent of the created policy, or policy and nested rules.', 'Policy description');
+    addHelp(
+        'policyDescription',
+        'Optional description of the intent of the created policy, or policy and nested rules.',
+        'Policy description'
+    );
 
     // Type
-    addHelp('policyType', 'Clarifies how subsequent rules should exist in relation to each other: <ul><li>AND: Each rule works independently from other rules in policy. One failing rule causes the policy to fail.</li><li>OR: All rules set within the policy work in relation to each other and only one rule passing is necessary for the policy to pass.</li></ul>', 'Policy type');
+    addHelp(
+        'policyType',
+        'Clarifies how subsequent rules should exist in relation to each other: \
+        <ul><li>AND: Each rule works independently from other rules in policy. One failing rule causes the policy to fail.</li> \
+        <li>OR: All rules set within the policy work in relation to each other and only one rule passing is necessary for the policy to pass.</li></ul>',
+        'Policy type'
+    );
 
     // License
-    addHelp('policyLicense', '<ul><li>Creative Commons Zero (1.0 or later): I like public domain</li><li>Creative Commons Attribution (4.0 or later): I want it permissive</li><li>Creative Commons Attribution-ShareAlike (4.0 or later): I care about sharing improvements</li><li>Other: a license not in the predefined list</li><footer>More information about how to choose a license for your policies may be found on <a href="https://creativecommons.org/share-your-work/" onclick="urlUtils.openInBrowser(event);">Creative Commons website</a></footer>', 'Policy license');
+    addHelp(
+        'policyLicense',
+        '<ul><li>Creative Commons Zero (1.0 or later): I like public domain</li> \
+        <li>Creative Commons Attribution (4.0 or later): I want it permissive</li> \
+        <li>Creative Commons Attribution-ShareAlike (4.0 or later): I care about sharing improvements</li> \
+        <li>Other: a license not in the predefined list</li></ul> \
+        <footer>More information about how to choose a license for your policies may be found on <a href="https://creativecommons.org/share-your-work/" onclick="urlUtils.openInBrowser(event);">Creative Commons website</a></footer>',
+        'Policy license');
 
     // Visibility
-    addHelp('policyVisibility', 'Allow your policy to be public and visible on the public policies list:<ul><li>Private: your policy won\'t be visible to others.</li><li>Public : your policy will be visible by others.</li></ul>', 'Policy visibility')
+    addHelp(
+        'policyVisibility',
+        'Allow your policy to be public and visible on the public policies list: \
+        <ul><li>Private: your policy won\'t be visible to others.</li> \
+        <li>Public : your policy will be visible by others.</li></ul>',
+        'Policy visibility'
+    );
 
     // Push to mco
-    popoverHelp.add('.policyPush', 'You can add your policy to MediaConchOnline.<ul><li>Public: the policy is added to your policies list and also to the public policy page to share it with everyone</li><li>Private: the policy is added to your policies list but is only available for you</li></ul>', 'Push policy', 'policyPush');
+    popoverHelp.add(
+        '.policyPush',
+        'You can add your policy to MediaConchOnline. \
+        <ul><li>Public: the policy is added to your policies list and also to the public policy page to share it with everyone</li> \
+        <li>Private: the policy is added to your policies list but is only available for you</li></ul>',
+        'Push policy',
+        'policyPush'
+    );
 }
 
 function policyRuleHelp() {
--- Source/Resource/html/js/policyUser.js
+++ Source/Resource/html/js/policyUser.js
@@ -24,8 +24,9 @@ var userConnectModal = (function() {
     }
 
     var formError = function(error) {
-
-        $('.userConnectModalError').html('<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Login fail, please check your login and password</div>');
+        $('.userConnectModalError').html('<div class="alert alert-danger alert-dismissible"> \
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> \
+        Login fail, please check your login and password</div>');
     }
 
     var setAction = function(action) {
