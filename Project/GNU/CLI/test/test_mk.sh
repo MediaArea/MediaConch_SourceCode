0dfb2718678a71c2dda80839b0aadbdaee99e23b
Maxime Gervais <gervais.maxime@gmail.com>
2017-06-21 09:56:11 +0200

    tests/matroska: ignore policy related tests
    + ignore "make" blocks in md files
    + handle more than one file per test
    
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/GNU/CLI/test/test_mk.sh
+++ Project/GNU/CLI/test/test_mk.sh
@@ -12,68 +12,60 @@ RCODE=0
 pushd "${FILES_DIRECTORY}"
 
 for FILE in *.md ; do
-    # skip test if there no .md file
-    if [ "${FILE}" == "*.md" ] ; then
-        exit 77
-    fi
+    TEST="$(grep -Eom1 '^# [0-9A-Z\-]+[0-9A-Z]' ${FILE} | grep -Eom1 '[0-9A-Z\-]+[0-9A-Z]')"
+    SCRIPT="$(perl -0777lne 'print for grep !/make/,/^```sh\s+(.*?)^```/mgs' ${FILE})"
 
-    TEST="$(grep -Eom1 '^# [0-9A-Z\-]+' ${FILE} | grep -Eom1 '[0-9A-Z\-]+')"
-    TESTFILE="$(grep -Eom1 '[0-9A-Z\-]+\.mkv' ${FILE})"
-    TESTNAME="${TESTFILE%.*}"
-    SCRIPT="$(sed -n '/```sh/,/```/{/```sh/!{/```/!p;};}' ${FILE})"
-
-    if [ "${TESTFILE}" == "" ] || [ "${SCRIPT}" == "" ] ; then
+    if [ -z "${SCRIPT}" ] || grep -iq "policy" ${FILE} ; then
         continue
     fi
 
-    if [ "${TESTNAME}" == "MKV-NUMERICAL-TAGS" ] ; then
-        TESTNAME="MKV-NUMERICAL-TAG"
+    if [ "${TEST}" == "MKV-NUMERICAL-TAGS" ] ; then
+        TEST="MKV-NUMERICAL-TAG"
     fi
 
     if [ "${TEST}" == "HEADER-ELEMENTS-WITHIN-MAXIDLENGTH" ] || \
-       [ "${TEST}" == "COLOUR-PRIMARY-CLARITY" ] || \
-       [ "${TEST}" == "INTERLACEMENT-CLARITY" ] || \
-       [ "${TEST}" == "SAMPLE-RANGE-CLARITY" ] || \
        [ "${TEST}" == "SEGMENTUID-PRESENT" ] || \
        [ "${TEST}" == "EBML-ELEM-START" ] || \
        [ "${TEST}" == "EBML-CRC-FIRST" ] ; then
         continue;
     fi
 
-    echo -e "#!/bin/sh\n${SCRIPT}" > "${TESTNAME}.sh"
-    sed -i'' 's/^xml /xmlstarlet /g' "${TESTNAME}.sh"
-    sh "${TESTNAME}.sh" &>/dev/null
+    echo "${SCRIPT}" > "${TEST}.sh"
+    sed -i'' 's/^xml /xmlstarlet /g' "${TEST}.sh"
+    sh "${TEST}.sh" &>/dev/null
+    rm "${TEST}.sh"
 
     # clean intermediate files
-    rm -f "${TESTNAME}.sh" "reference.xml" "reference.mkv"
+    rm -f "reference.xml" "reference.mkv"
 
-    if [ ! -e "${TESTFILE}" ] || [ "$(stat -c %s ${TESTFILE})" -eq 0 ] ; then
-        echo "INVALID: ${TESTFILE}" >&9
+    for TESTFILE in ${TEST}*.mkv ; do
+        if [ ! -e "${TESTFILE}" ] || [ "$(stat -c %s ${TESTFILE})" -eq 0 ] ; then
+            echo "INVALID: ${TESTFILE}" >&9
+            rm -f "${TESTFILE}"
+            RCODE=1
+            continue
+        fi
+
+        DATA="$(${MC} -fx ${TESTFILE})"
+
+        # test if the check fail as expected
+        T1=`echo "${DATA}" | xmllint --xpath \
+        "boolean(//*/*[@icid and @icid=\"${TEST}\" and @fail_count and @fail_count!=\"0\"])" -`
+
+        # test if other check fail
+        T2=`echo "${DATA}" | xmllint --xpath \
+        "boolean(//*/*[@icid and @icid!=\"${TEST}\" and @fail_count and @fail_count!=\"0\"])" -`
+
+        if [ "${T1}" != "true" ] || [ "${T2}" != "false" ] ; then
+            echo "NOK: ${TEST}" >&9
+            rm -f "${TESTFILE}"
+            RCODE=1
+            continue
+        fi
+
         rm -f "${TESTFILE}"
-        RCODE=1
-        continue
-    fi
-
-    DATA="$(${MC} -fx ${TESTFILE})"
-
-    # test if the check fails as expected
-    T1=`echo "${DATA}" | xmllint --xpath \
-    "boolean(//*/*[@icid and @icid=\"${TESTNAME}\" and @fail_count and @fail_count!=\"0\"])" -`
-
-    # test if other check fails
-    T2=`echo "${DATA}" | xmllint --xpath \
-    "boolean(//*/*[@icid and @icid!=\"${TESTNAME}\" and @fail_count and @fail_count!=\"0\"])" -`
-
-    if [ "${T1}" != "true" ] || [ "${T2}" != "false" ] ; then
-        echo "NOK: ${TEST}" >&9
-        rm -f "${TESTFILE}"
-        RCODE=1
-        continue
-    fi
-
-    rm -f "${TESTFILE}"
-
-    echo "OK: ${TEST}" >&9
+        echo "OK: ${TEST}" >&9
+    done
 done
 
 popd
